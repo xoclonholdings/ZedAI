1a2bf4c8d68bd8052d66d12ed0f7f9ba1b3d06a4
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-03-26 06:32:26 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { UseAuth } from "@/components/auth/UseAuth";
+import { useAuth } from "@/components/auth/UseAuth";
 import NotFound from "@/pages/not-found";
 import Chat from "@/pages/chat";
 import Login from "@/pages/login";
