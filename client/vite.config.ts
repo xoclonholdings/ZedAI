bf18631c2630895e7bfda42ae9e7e5eec36da1f0
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-06-25 01:03:21 -0400

    Fix Vite shared alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@shared": path.resolve(__dirname, "../shared"),
       "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
